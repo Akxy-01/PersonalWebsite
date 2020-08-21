d4bd4374dfe3883da97cbdf11be27e90fd6f78f0
Akxy-01 <akshatha01sudhir@gmail.com>
2020-08-21 01:16:01 -0700

    Updates

---
+++ precache-manifest.eb1c53af416b33fad314aaecb3a88cac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f999511a587e29590f6038913169a9a",
+    "url": "/PersonalWebsite/index.html"
+  },
+  {
+    "revision": "7db66eb764b66e50c008",
+    "url": "/PersonalWebsite/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "25fe14a89c8ce89f4ef7",
+    "url": "/PersonalWebsite/static/css/main.aad4fd5a.chunk.css"
+  },
+  {
+    "revision": "7db66eb764b66e50c008",
+    "url": "/PersonalWebsite/static/js/2.319f10cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PersonalWebsite/static/js/2.319f10cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fe14a89c8ce89f4ef7",
+    "url": "/PersonalWebsite/static/js/main.1cc494e5.chunk.js"
+  },
+  {
+    "revision": "347f0e994e1e3318f567",
+    "url": "/PersonalWebsite/static/js/runtime-main.2b01367d.js"
+  },
+  {
+    "revision": "b53070e043c57ff5d0de0b57217fa3e6",
+    "url": "/PersonalWebsite/static/media/bucketshaper.b53070e0.png"
+  },
+  {
+    "revision": "34df1aece3d6da0db2a928acd7a5b206",
+    "url": "/PersonalWebsite/static/media/halmagame.34df1aec.jpg"
+  },
+  {
+    "revision": "7bca762bccf6a51987aee6dca3754663",
+    "url": "/PersonalWebsite/static/media/mypic.7bca762b.jpeg"
+  },
+  {
+    "revision": "8c915dbeda265156f9e2df6f46e4fafc",
+    "url": "/PersonalWebsite/static/media/searchagent.8c915dbe.jpg"
+  }
+]);
